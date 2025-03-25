a1fec1a808d72e38893447222fe89665e5793b51
Silas <silasandrade.code@gmail.com>
2025-03-25 20:33:11 -0300

    ✨ feat: Add navbar model

--- base/views.py
+++ base/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def home(request):
     return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('<h1>Salas</h1>')
- 
+    return render(request, 'room.html')
