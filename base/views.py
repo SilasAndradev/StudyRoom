c6e5df7ac4233f7a11878c8e9a76038b8b72f2a7
Silas <silasandrade.code@gmail.com>
2025-03-25 21:02:21 -0300

    ✨ feat: Organizei os templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1,'name':'Vamos aprender Python!'},
+    {'id':2,'name':'Vamos aprender Django!'},
+    {'id':3,'name':'Vamos aprender Django Rest FrameWork!'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html', {'rooms': rooms})
